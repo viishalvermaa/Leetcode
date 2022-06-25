1c6466f8c4eca5fea7bcba934fb19366f774e0c3
Vishal Verma <64699822+vishal3008@users.noreply.github.com>
2022-06-25 22:53:15 +0530

    Time: 2 ms (82.16%), Space: 8.3 MB (79.43%) - LeetHub

---
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int n=arr.size();
+        int ans=0;
+        for(int i=0;i<n;i++)
+            ans+=(((i+1)*(n-i)+1)/2)*arr[i];
+        return ans;
+    }
+};

b31ce55bc10925396206da90ecd8b625163298b2
Vishal Verma <64699822+vishal3008@users.noreply.github.com>
2022-06-29 00:09:15 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (67.45%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,12 +4,14 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int lo = 1, hi = n, mid;
-        while (lo < hi) {
-            mid = lo + (hi - lo) / 2;
-            if (isBadVersion(mid)) hi = mid;
-            else lo = mid+1;
+        int l = 1, h = n, mid;
+        while (l < h) {
+            mid = l + (h - l) / 2;
+            if (isBadVersion(mid)) 
+                h = mid;
+            else 
+                l = mid+1;
         }
-        return lo;
+        return l;
     }
 };

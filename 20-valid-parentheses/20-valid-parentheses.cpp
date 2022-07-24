5a891b6e6ceaddc64f8556eec3a2c2c26c3fa759
Vishal Verma <64699822+viishalvermaa@users.noreply.github.com>
2022-07-24 22:58:29 +0530

    Time: 5 ms (19.43%), Space: 6.4 MB (50.82%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n=s.size();
+        
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
+                st.push(s[i]);
+            else{
+                if(st.empty())
+                    return false;
+                char c=st.top();
+                st.pop();
+                if((s[i]==')' && c=='(') || (s[i]==']' && c=='[') || (s[i]=='}' && c=='{'))
+                    continue;
+                else
+                    return false;
+            }
+        }
+        if(!st.empty())
+            return false;
+        return true;
+    }
+};
